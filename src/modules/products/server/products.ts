56555b35ca23395e4d526dcde35cca1966692850
Harjot <anandharjot29@gmail.com>
2025-05-23 16:16:15 +0530

    12-filtering-products fix 2

--- src/modules/products/server/products.ts
+++ src/modules/products/server/products.ts
@@ -14,16 +14,23 @@ export const productsRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const where: Where = {};
-      if(input.minPrice){
+      const where: Where = {
+        price: {},
+      };
+
+      if(input.minPrice && input.maxPrice){
         where.price = {
-          ...where.price,
-          greater_than_equal: input.minPrice
+          less_than_equal: input.maxPrice,
+          greater_than_equal: input.minPrice,
+        }
+      }else if(input.minPrice){
+        where.price = {
+          greater_than_equal: input.minPrice,
         }
       }
-      if(input.maxPrice){
+      else if(input.maxPrice){
         where.price = {
-          ...where.price,
+          
           less_than_equal: input.maxPrice,
         }
       }

897cac3a120ecb53aa6b82b5cfb90d4697aa5fa4
Harjot <anandharjot29@gmail.com>
2025-05-23 16:07:38 +0530

    12-filtering products

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,28 +1,44 @@
-import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
-import { caller, getQueryClient , trpc } from "@/trpc/server";
+import { ProductFilters } from "@/modules/products/ui/components/product-filters";
+import {
+  ProductList,
+  ProductListSkeleton,
+} from "@/modules/products/ui/components/product-list";
+import { caller, getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
 
-interface Props{
-    params : Promise<{
-        category: string;
-    }>
+interface Props {
+  params: Promise<{
+    category: string;
+  }>;
 }
 
-const page = async ({params} : Props) => {
-    const {category } = await params;
-  
+const page = async ({ params }: Props) => {
+  const { category } = await params;
+
   const queryCLient = getQueryClient();
-  void queryCLient.prefetchQuery(trpc.products.getMany.queryOptions({
-    category,
-  }));
+  void queryCLient.prefetchQuery(
+    trpc.products.getMany.queryOptions({
+      category,
+    })
+  );
   return (
-   <HydrationBoundary state={dehydrate(queryCLient)}>
-    <Suspense fallback={<ProductListSkeleton />}>
-      <ProductList category={category} />
-    </Suspense>
-   </HydrationBoundary>
-  )
-}
-export default page
-
+    <HydrationBoundary state={dehydrate(queryCLient)}>
+      <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-06 gap-x-12">
+          <div className="lg:col-span-2 xl:col-span-2">
+            <ProductFilters />
+          </div>
+          
+          <div className="lg:col-span-4 xl:col-span-6">
+            <Suspense fallback={<ProductListSkeleton />}>
+            <ProductList category={category} />
+          </Suspense>
+          </div>
+          
+        </div>
+      </div>
+    </HydrationBoundary>
+  );
+};
+export default page;

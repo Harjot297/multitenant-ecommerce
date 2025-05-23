cc7404a3ebf6a98b061f64f755d75bfcaf562199
Harjot <anandharjot29@gmail.com>
2025-05-23 20:45:59 +0530

    13-api-filters-sorting

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,10 +3,12 @@ import { authRouter } from '@/modules/auth/server/procedures';
 import { createTRPCRouter } from '../init';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { productsRouter } from '@/modules/products/server/products';
+import { tagsRouter } from '@/modules/tags/server/tags';
 export const appRouter = createTRPCRouter({
   auth : authRouter,
   categories: categoriesRouter,
   products: productsRouter,
+  tags: tagsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;

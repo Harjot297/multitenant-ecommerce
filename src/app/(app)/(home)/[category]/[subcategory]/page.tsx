82686dfc2f445aa38dc3e221b0c4662f3d9f2f60
Harjot <anandharjot29@gmail.com>
2025-05-24 17:29:59 +0530

    14-product-list-ui

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,27 +1,33 @@
-import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
-import { caller, getQueryClient , trpc } from "@/trpc/server";
+import { loadProductFilters } from "@/modules/products/search-params";
+
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { getQueryClient , trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { SearchParams } from "nuqs";
+
 
 interface Props{
     params : Promise<{
         subcategory: string;
-    }>
+    }>,
+    searchParams : Promise<SearchParams>;
 }
 
-const page = async ({params} : Props) => {
-    const {subcategory } = await params;
-  
+const page = async ({ params , searchParams}: Props) => {
+  const { subcategory } = await params;
+  const filters = await loadProductFilters(searchParams);
+
   const queryCLient = getQueryClient();
-  void queryCLient.prefetchQuery(trpc.products.getMany.queryOptions({
-    category: subcategory,
-  }));
+  void queryCLient.prefetchQuery(
+    trpc.products.getMany.queryOptions({
+      category: subcategory,
+      ...filters,
+    })
+  );
   return (
-   <HydrationBoundary state={dehydrate(queryCLient)}>
-    <Suspense fallback={<ProductListSkeleton />}>
-      <ProductList category={subcategory} />
-    </Suspense>
-   </HydrationBoundary>
-  )
-}
-export default page
+    <HydrationBoundary state={dehydrate(queryCLient)}>
+      <ProductListView category={subcategory} />
+    </HydrationBoundary>
+  );
+};
+export default page;
--- src/modules/products/server/products.ts
+++ src/modules/products/server/products.ts
@@ -2,13 +2,16 @@ import z from "zod";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import type { Sort, Where } from "payload";
 import CategoryDropdown from "@/modules/home/ui/components/search-filters/CategoryDropdown";
-import { Category } from "@/payload-types";
+import { Category, Media } from "@/payload-types";
 import { sortValues } from "../search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
+        cursor : z.number().default(1),
+        limit: z.number().default(DEFAULT_LIMIT),
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
@@ -97,8 +100,16 @@ export const productsRouter = createTRPCRouter({
         depth: 1, // ensures image and category is populated
         where,
         sort,
+        page: input.cursor,
+        limit: input.limit,
       });
 
-      return data;
+      return {
+        ...data,
+        docs: data.docs.map((doc) => ({
+          ...doc,
+          image: doc.image as Media | null,
+        }))
+      }
     }),
 });
